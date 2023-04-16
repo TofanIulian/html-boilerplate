e2ae1639d8250ba5258ba41cb39fa0e87847fcd5
Iulian Tofan <itn@tanacorp.com>
2023-04-16 10:50:31 +0300

    Create the first 3 sections

--- src/script.js
+++ src/script.js
@@ -1,1 +1,26 @@
 console.log('Hello World');
+
+AOS.init();
+
+// Accordion Logic
+// TODO: Problema cu max-height-ul la responsive - fixat
+// TODO: trebuie ales un max-height potrivit mai mare decat orice
+var acc = document.getElementsByClassName('accordion-header');
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = '800px';
+    }
+  });
+}
+// End of Accordion Logic
+
+// Smooth Slider
+
+// End of Smooth Slider
